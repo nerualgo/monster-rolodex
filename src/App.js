6a53f78895869bff6c95a24decc058d29bcd9f80
Lauren Go <laurengo@Laurens-Mini.fios-router.home>
2022-10-07 16:34:18 -0400

    39

--- src/App.js
+++ src/App.js
@@ -32,6 +32,12 @@ class App extends Component {
     console.log('render');
     return (
       <div className="App">
+      <input className='search-box' 
+      type='search' 
+      placeholder='search monsters' 
+      onChange={(event) => {
+        console.log(event.target.value);
+      }}/>
         {this.state.monsters.map((monster) =>{
           return (
             <div key={monster.id}>
